75edc7659bcbcf8d04c148951a72d88c3a9a55c9
VolodymyrRushchak <93164951+VolodymyrRushchak@users.noreply.github.com>
2022-06-19 17:16:20 +0300

    Improving the structure

--- riverServer/src/main/java/ua/lviv/iot/riverServer/dataaccess/file/CSVFileStorage.java
+++ riverServer/src/main/java/ua/lviv/iot/riverServer/dataaccess/file/CSVFileStorage.java
@@ -1,5 +1,7 @@
 package ua.lviv.iot.riverServer.dataaccess.file;
 
+import org.springframework.stereotype.Component;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -10,12 +12,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+@Component
 public final class CSVFileStorage {
 
-    private CSVFileStorage() {
-    }
-
-    public static void writeToFile(final String headers, final  String toCSV, final  String type) throws IOException {
+    public void writeToFile(final String headers, final  String toCSV, final  String type) throws IOException {
         String path = type + "-" + LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE) + ".csv";
         if (!Files.exists(Path.of(path))) {
             try (FileWriter fileWriter = new FileWriter(path, StandardCharsets.UTF_8)) {
@@ -29,7 +30,7 @@ public final class CSVFileStorage {
         }
     }
 
-    public static void deleteFromFile(final Long id, final String type) throws IOException {
+    public void deleteFromFile(final Long id, final String type) throws IOException {
         for (int i = 1; i <= LocalDate.now().getDayOfMonth(); i++) {
             Path path = Path.of(type + "-" + LocalDate.now().format(DateTimeFormatter.ofPattern("uuuu-MM-"))
                     + String.format("%02d", i) + ".csv");
