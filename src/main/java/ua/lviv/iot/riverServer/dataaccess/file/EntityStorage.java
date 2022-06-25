1599436f8e783c1a2ec2257358d73f9e578f144a
Volodymyr Rushchak <rushvolod@gmail.com>
2022-06-25 18:08:32 +0300

    A little of cleaning

--- src/main/java/ua/lviv/iot/riverServer/dataaccess/file/EntityStorage.java
+++ src/main/java/ua/lviv/iot/riverServer/dataaccess/file/EntityStorage.java
@@ -16,33 +16,8 @@ public abstract class EntityStorage {
     protected Long idHolder = 0L;
     protected String workingDirectory;
 
-    private String filePath(final int day) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        String stringDate = dateFormat.format(calendar.getTime());
-        return workingDirectory + File.separator + getType() + "-" + stringDate + ".csv";
-    }
-
     public void initHashMap() {
         iterateOverCurrentMonth();
-    }
-
-    private void iterateOverCurrentMonth() {
-        for (int i = 1; i <= LocalDate.now().getDayOfMonth(); ++i) {
-            String path = filePath(i);
-            File file = new File(path);
-            if (file.exists()) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
-                    List<String> lines = reader.lines().toList();
-                    for (int j = 1; j < lines.size(); j++) {
-                        addToHash(lines.get(j));
-                    }
-                } catch (IOException exc) {
-                    exc.printStackTrace();
-                }
-            }
-        }
     }
 
     public void create(final Entity entity) throws IOException {
@@ -57,6 +32,40 @@ public abstract class EntityStorage {
             try (FileWriter fileWriter = new FileWriter(path, StandardCharsets.UTF_8)) {
                 fileWriter.write(entity.getHeaders() + System.lineSeparator());
                 fileWriter.write(entity.toCSV() + System.lineSeparator());
+            }
+        }
+    }
+
+    public Boolean update(final Entity entity, final Long id) throws IOException {
+        return changeFile(entity, id, true);
+    }
+
+    public Boolean delete(final Long id) throws IOException {
+        return changeFile(null, id, false);
+    }
+
+    public void setWorkingDirectory(final String dir) {
+        idHolder = 0L;
+        workingDirectory = dir;
+    }
+
+    public abstract String getType();
+
+    public abstract void addToHash(String record);
+
+    private void iterateOverCurrentMonth() {
+        for (int i = 1; i <= LocalDate.now().getDayOfMonth(); ++i) {
+            String path = filePath(i);
+            File file = new File(path);
+            if (file.exists()) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
+                    List<String> lines = reader.lines().toList();
+                    for (int j = 1; j < lines.size(); j++) {
+                        addToHash(lines.get(j));
+                    }
+                } catch (IOException exc) {
+                    exc.printStackTrace();
+                }
             }
         }
     }
@@ -94,21 +103,12 @@ public abstract class EntityStorage {
         return false;
     }
 
-    public Boolean update(final Entity entity, final Long id) throws IOException {
-        return changeFile(entity, id, true);
+    private String filePath(final int day) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        String stringDate = dateFormat.format(calendar.getTime());
+        return workingDirectory + File.separator + getType() + "-" + stringDate + ".csv";
     }
-
-    public Boolean delete(final Long id) throws IOException {
-        return changeFile(null, id, false);
-    }
-
-    public void setWorkingDirectory(final String dir) {
-        idHolder = 0L;
-        workingDirectory = dir;
-    }
-
-    public abstract String getType();
-
-    public abstract void addToHash(String record);
 
 }
